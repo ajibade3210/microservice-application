5e746182fffef8db012b438966b375ffd79c857b
ajibade3210 <ajibade69th@gmail.com>
2022-07-23 18:57:41 +0100

    Adding moderation flag:

--- query/index.js
+++ query/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
@@ -22,29 +23,56 @@ const posts = {};
  * }
  */
 
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
-  if (type === "PostCreated") {
-    const { id, title } = data;
-    posts[id] = { id, title, comments: [] };
-  }
 
-  if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+  handleEvent(type, data);
 
-    const post = posts[postId];
-    post.comments.push({ id, content });
-  }
-
-  console.log(posts);
   res.send({});
 });
 
-//Link Query Event To Client
-app.listen(7000, () => {
-  console.log("Listening on Port: 7000");
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
